c11496c197ddeb6220b6d1d5e898b57490e065d1
Pavel <paulcodeman@gmail.com>
2023-03-27 15:29:45 +0200

    Update hash.js

--- hash.js
+++ hash.js
@@ -9,3 +9,37 @@ function hash1(text)
   }
   return (len << 20) | sum;
 }
+
+function hash2(text)
+{
+  var salt = Math.floor(Math.random() * 100) + 1; // добавить случайный salt
+  var len = text.length, sum = 0, save = 0;
+  for (var i = 0; i < len; i++)
+  {
+    if (save > text.charCodeAt(i)) sum += i;
+    else save = text.charCodeAt(i);
+    sum += save;
+  }
+  return (len << 20) | sum | salt; // объединить salt с результатом
+}
+
+
+function hash3(text)
+{
+  var sha256 = new Hashes.SHA256; // использовать SHA-256
+  return sha256.hex(text); // вернуть хэш в шестнадцатеричном формате
+}
+
+
+function hash4(text)
+{
+  var len = text.length, sum = 0, save = 0;
+  for (var i = 0; i < len; i++)
+  {
+    if (save > text.charCodeAt(i)) sum += i;
+    else save = text.charCodeAt(i);
+    sum += save;
+  }
+  var mask = 0xFACB; // битовая маска
+  return (len << 20) ^ sum ^ mask; // использовать xor для объединения маски
+}
